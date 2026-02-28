cb6085abdfa74c52f003e83d168158349ba97ba5
Sam Rook <samr2411@gmail.com>
2026-02-28 17:53:28 +0000

    add global HTTP log middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +24,20 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        Http::globalRequestMiddleware(function (Request $request): Request {
+            $method = $request->getMethod();
+            $uri = (string) $request->getUri();
+            $body = $request->getBody();
+
+            Log::debug("{$method} {$uri}", ['request' => [
+                'headers' => $request->getHeaders(),
+                'body' => $body->getContents(),
+            ]]);
+
+            $body->rewind();
+
+            return $request;
+        });
     }
 }
